c8e49c46d795b5c6b0431250c501c96e3ddbcf9a
aslan <as.ashabokov98@gmail.com>
2021-10-11 09:53:43 +0300

    hw5 started

---
+++ task_5/task_A.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+// #include <vector>
+
+using namespace std;
+
+int COMMAND_LENGTH = 7;
+int MAX_INPUT_LENGTH = 1e6;
+
+template<class Key, class Value>
+class Hashtable {
+
+    struct Node {
+        Value value;
+        Key key;
+        bool is_empty;
+        bool is_deleted;
+
+        Node () {
+            this->value = Value();
+            this->key = Key();
+            this->is_empty = true;
+            this->is_deleted = false;
+        }
+    };
+
+    int INITIAL_CAPACITY = 1e6;
+
+    Node *data;
+    int capacity;
+    int size;
+    int empty_count;
+
+    int hash_by_key(Key &key) {
+        return (key % this->capacity);
+    }
+
+    void reshape(int new_capacity) {
+        Node *old_data = this->data;
+        int size = this->size;
+        this->empty_count = new_capacity - this->size;
+        this->capacity = new_capacity;
+        this->data = new Node[new_capacity];
+
+        for(int i = 0; i < size; i++) {
+            if (!old_data[i].is_empty && !old_data[i].is_deleted) {
+                this->insert_item(old_data[i].key, old_data[i].value);
+            }
+        }
+
+        delete[] old_data;
+    }
+
+public:
+
+    Hashtable() {
+        this->capacity = INITIAL_CAPACITY;
+        this->size = 0;
+        this->empty_count = this->capacity;
+        this->data = new Node[this->capacity];
+    }
+
+    void insert_item(Key &key, Value &value) {
+        if (this->empty_count == 0) {
+            this->reshape(this->size * 2 + 1);
+        }
+
+        int position = this->hash_by_key(key);
+
+        while (!this->data[position].is_empty && !this->data[position].is_deleted) {
+            if (this->data[position].key == key) {
+                return;
+            }
+            
+            position++;
+        }
+
+        if (!this->data[position].is_deleted && this->data[position].key == key)
+
+        this->data[position].value = value;
+        this->data[position].key = key;
+        this->data[position].is_empty = false;
+        this->data[position].is_deleted = false;
+        this->size++;
+        this->empty_count--;
+    }
+
+    void delete_item(Key &key) {
+        int position = this->hash_by_key(key);
+
+        while (!this->data[position].is_empty && this->data[position].key != key) {
+            if (this->data[position].is_deleted && this->data[position].key == key) {
+                return;
+            }
+
+            position++;
+        }
+
+        if (!this->data[position].is_empty) {
+            this->data[position].is_deleted = true;
+            this->data[position].is_empty = false;
+            this->size--;
+        }
+    }
+
+    bool exists(Key &key) {
+        int position = this->hash_by_key(key);
+
+        while (!this->data[position].is_empty && this->data[position].key != key || 
+                this->data[position].is_deleted && this->data[position].key == key) {
+            position++;
+        }
+
+        if (!this->data[position].is_empty && !this->data[position].is_deleted) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    int get_size() {
+        return this->size;
+    }
+
+    void print() {
+        cout << "===>" << endl;
+
+        for (int i = 0; i < this->capacity; i++) {
+            if (!this->data[i].is_empty) {
+                cout << i << " " << this->data[i].key << " " << this->data[i].value << " "  << this->data[i].is_deleted << endl;
+            }
+        }
+
+        cout << "<===" << endl;
+    }
+
+    ~Hashtable() {
+        delete[] this->data;
+    }
+};
+
+template<class Value>
+class Set {
+    Hashtable<Value, Value> *data;
+
+public:
+
+    Set() {
+        this->data = new Hashtable<Value, Value>();
+    }
+
+    void insert_item(Value &value) {
+        this->data->insert_item(value, value);
+        // this->data->print();
+    }
+
+    void delete_item(Value &value) {
+        this->data->delete_item(value);
+        // this->data->print();
+    }
+
+    bool exists(Value &value) {
+        // this->data->print();
+        return this->data->exists(value);
+    }
+
+    ~Set() {
+        delete data;
+    }
+};
+
+int main() {
+    Set<int> set;
+    char cmd[COMMAND_LENGTH];
+    int value;
+    int res_count = 0;
+    bool *result = new bool[MAX_INPUT_LENGTH];
+
+    while (scanf("%s %d\n", cmd, &value) != EOF) {
+        switch (cmd[0]) {
+        case 'i':
+            set.insert_item(value);
+            break;
+        case 'd':
+            set.delete_item(value);
+            break;
+        case 'e':
+            // cout << (set.exists(value) ? "true" : "false") << endl;
+            result[res_count++] = set.exists(value);
+            break;
+        default:
+            cout << "Something went wrong" << endl;
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < res_count; i++) {
+        printf("%s\n", result[i] ? "true" : "false");
+    }
+
+    delete[] result;
+
+    return 0;
+}
+
+//  int main() {
+//     Hashtable<int, int> set;
+//     char cmd[COMMAND_LENGTH];
+//     int value;
+//     int res_count = 0;
+//     bool *result = new bool[MAX_INPUT_LENGTH];
+
+//     while (scanf("%s %d\n", cmd, &value) != EOF) {
+//         switch (cmd[0]) {
+//         case 'i':
+//             set.insert_item(value, value);
+//             break;
+//         case 'd':
+//             set.delete_item(value);
+//             break;
+//         case 'e':
+//             // cout << (set.exists(value) ? "true" : "false") << endl;
+//             result[res_count++] = set.exists(value);
+//             break;
+//         default:
+//             cout << "Something went wrong" << endl;
+//             return 1;
+//         }
+//     }
+
+//     for (int i = 0; i < res_count; i++) {
+//         printf("%s\n", result[i] ? "true" : "false");
+//     }
+
+//     delete[] result;
+
+//     return 0;
+// }
